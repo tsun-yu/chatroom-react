d032dc141f750f81f8117174b68fee303905af61
Wayne <tsun0227@yahoo.com.tw>
2024-05-11 14:44:04 +0800

    change style of login switch

--- src/components/loginPage/LoginSection.tsx
+++ src/components/loginPage/LoginSection.tsx
@@ -170,7 +170,7 @@ function LoginSection(props: LoginSectionProps) {
           <></>
         )}
         <div className="login__input">
-          <Button label={label.toUpperCase()} onClick={handleLoginClick} />
+          <Button label={label} onClick={handleLoginClick} />
         </div>
       </LoginElement>
     </>
